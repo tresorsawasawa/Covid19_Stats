60380325f500040557032127f5e1d29db25fada2
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-16 13:33:17 +0200

    Import the redux store and link react and redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './scss/index.scss';
+import store from './redux/configStore';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
