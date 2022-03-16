12c2299a591d4bde89ad4abca08cb0436243f08d
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-16 14:35:52 +0200

    Add functionality to get coutris maps

---
+++ src/utils/countryMap.js
@@ -0,0 +1,16 @@
+import getCountryTwoLetterCode from './countriesAbrev';
+
+const MAP_BASE_URL =
+  'https://raw.githubusercontent.com/tresorsawasawa/mapsicon/master/all';
+
+const getCountryMapUrl = (countryName, mapSize) => {
+  const countryAbrevCode = getCountryTwoLetterCode(countryName);
+
+  if (countryAbrevCode === undefined) {
+    return 'https://raw.githubusercontent.com/tresorsawasawa/mapsicon/master/all/af/128.png';
+  }
+
+  return `${MAP_BASE_URL}/${countryAbrevCode}.png`;
+};
+
+export default getCountryMapUrl;
